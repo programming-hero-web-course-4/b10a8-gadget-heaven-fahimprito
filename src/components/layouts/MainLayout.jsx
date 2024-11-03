c289295f3fb32c5182a119e925d025d78de57c59
fahimprito <ahmmedahmmed75@gmail.com>
2024-11-03 23:34:39 +0600

    initial setup

---
+++ src/components/layouts/MainLayout.jsx
@@ -0,0 +1,17 @@
+import Footer from "../Footer/Footer";
+import Navbar from "../Navbar/Navbar";
+
+
+const MainLayout = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <div>
+                
+            </div>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default MainLayout;
